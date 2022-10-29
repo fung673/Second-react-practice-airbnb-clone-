014c55b52aa0d2644dbc0efdafd30169e2e6b841
unknown <alexfung1234@gmail.com>
2022-10-29 16:55:57 +0800

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './component/nav';
 import { Card } from './component/card';
+import data from './data'
 
 export default function App() {
-  //<Hero />//
-  return (<div>
-    <Navbar />
-    <Card />
-  </div>)
+  // <Hero />
+  const cards = data.map(item => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        country={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    )
+  })
 
+  return (
+    <div>
+      <Navbar />
+      {cards}
+    </div>
+  )
 }
--- src/component/card.js
+++ src/component/card.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 export function Card(props) {
- return <div className="card"><img className="card--image" src={require('../images/katie-zaferes.png')} alt="katie-zaferes" />
+ return <div className="card"><img className="card--image" src={require(`../images/${props.img}`)} alt="katie-zaferes" />
   <div className="card--stats"><img className="card--star" src={require('../images/star.png')} alt="star" />
-   <span>5.0</span>
-   <span className="grey">(6)．</span>
-   <span className="grey">USA</span>
+   <span>{props.rating}</span>
+   <span className="grey">({props.reviewCount})．</span>
+   <span className="grey">{props.country}</span>
   </div>
-  <p>Life lesson with Katie Zaferes</p>
-  <p><span className="bold">From $136</span> / person</p>
+  <p>{props.title}</p>
+  <p><span className="bold">From ${props.price}</span> / person</p>
  </div>
 }
