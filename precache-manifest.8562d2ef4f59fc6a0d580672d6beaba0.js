cc3e3258fa5ac88e5e3a2c42755805b186145c58
unknown <alexfung1234@gmail.com>
2022-10-29 16:55:23 +0800

    Updates

---
+++ precache-manifest.8562d2ef4f59fc6a0d580672d6beaba0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a1452c5c18b824576f3",
+    "url": "/Second-react-practice-airbnb-clone-/static/css/main.ef550446.chunk.css"
+  },
+  {
+    "revision": "8a1452c5c18b824576f3",
+    "url": "/Second-react-practice-airbnb-clone-/static/js/main.8a1452c5.chunk.js"
+  },
+  {
+    "revision": "0d93543778ab7aa513f1",
+    "url": "/Second-react-practice-airbnb-clone-/static/js/1.0d935437.chunk.js"
+  },
+  {
+    "revision": "aabba7c1d64cbeddf66f",
+    "url": "/Second-react-practice-airbnb-clone-/static/js/runtime~main.aabba7c1.js"
+  },
+  {
+    "revision": "ad3f359159909c2b697da70f4baaa7db",
+    "url": "/Second-react-practice-airbnb-clone-/static/media/logo.ad3f3591.png"
+  },
+  {
+    "revision": "63567cd6235a874588c6ee3b2ce74eb2",
+    "url": "/Second-react-practice-airbnb-clone-/static/media/airbnb-logo.63567cd6.png"
+  },
+  {
+    "revision": "e9df633cbd05ee6f834900c0735f9d02",
+    "url": "/Second-react-practice-airbnb-clone-/static/media/grid.e9df633c.png"
+  },
+  {
+    "revision": "24df2e288eafb12a05291cb01cb5a597",
+    "url": "/Second-react-practice-airbnb-clone-/static/media/katie-zaferes.24df2e28.png"
+  },
+  {
+    "revision": "5c20bb07050dde895f19c12b869d9932",
+    "url": "/Second-react-practice-airbnb-clone-/static/media/mountain-bike.5c20bb07.png"
+  },
+  {
+    "revision": "0e83f5a0f847c2363202f3a1be3bbd27",
+    "url": "/Second-react-practice-airbnb-clone-/static/media/photo-grid.0e83f5a0.png"
+  },
+  {
+    "revision": "a49bced69bcb2ffdb564f062d63651b1",
+    "url": "/Second-react-practice-airbnb-clone-/static/media/wedding-photography.a49bced6.png"
+  },
+  {
+    "revision": "ac2cf25b1a86bfd6211a3eb9c4dbd4b1",
+    "url": "/Second-react-practice-airbnb-clone-/index.html"
+  }
+];
