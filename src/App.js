63dd397b71a3a10e8e801d22418598430ee32c01
unknown <alexfung1234@gmail.com>
2022-10-30 13:53:28 +0800

    finished

--- src/App.js
+++ src/App.js
@@ -9,15 +9,8 @@ export default function App() {
   const cards = data.map(item => {
     return (
       <Card
-
         key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        country={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
+        item={item}
       />
     )
   })
--- src/component/card.js
+++ src/component/card.js
@@ -1,24 +1,27 @@
 import React from "react";
 
 export function Card(props) {
- let badgeText
- if (props.openSpots === 0) {
-  badgeText = "SOLD OUT"
- } else if (props.country === "Online") {
-  badgeText = "ONLINE"
- }
+  let badgeText
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT"
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE"
+  }
 
 
- return (
-  <div className="card">
-   {badgeText && <div className="card--badge">{badgeText}</div>}
-   <img className="card--image" src={require(`../images/${props.img}`)} alt="katie-zaferes" />
-   <div className="card--stats"><img className="card--star" src={require('../images/star.png')} alt="star" />
-    <span>{props.rating}</span>
-    <span className="grey">({props.reviewCount})．</span>
-    <span className="grey">{props.country}</span>
-   </div>
-   <p className="card--title">{props.title}</p>
-   <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
-  </div>)
+
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={require(`../images/${props.item.coverImg}`)} className="card--image" />
+      <div className="card--stats">
+        <img src={require("../images/star.png")} className="card--star" />
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
+      </div>
+      <p className="card--title">{props.item.title}</p>
+      <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
+    </div>
+  )
 }
